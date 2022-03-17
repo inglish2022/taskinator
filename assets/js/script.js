32b20e6a62a733421c57f270e44d014073fe964a
Inglish Foust <inglish.foust5@gmail.com>
2022-03-17 18:16:06 -0400

    add task form

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var createTaskHandler = function() {
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  }
+  
 
 buttonEl.addEventListener("click", createTaskHandler);
-
+}
