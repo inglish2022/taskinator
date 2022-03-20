c190150b6a8b54acbbd33e49a4834b42bad82768
Inglish Foust <inglish.foust5@gmail.com>
2022-03-20 17:33:40 -0400

    fixed issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,12 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput)  {
+        alert("You need to fill out the task form!");
+        return false;
+        formEl.reset();
+    }
+
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -24,7 +30,7 @@ var taskFormHandler = function(event) {
         //create div to" hold task infor and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
     //add entire list item to list
