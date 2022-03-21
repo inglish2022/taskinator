7832c484d563e877b825bc8042ad0286a0961e06
Inglish Foust <inglish.foust5@gmail.com>
2022-03-21 17:07:39 -0400

    more mistakes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,42 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
 
+var completeEditTask = function(taskName, taskType, taskId)  {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    
+    //set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!");
+
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
+};
+
 var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    var isEdit = formEl.hasAttribute("data-task-id");
+    
+    //PUT THIS BELOW VAR ISEDIT = ...in taskFormHandler
+
+    //has data attribute, so get task id and call funciton to complet edit program
+    if (isEdit)  {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+    }
+
+    //no data attribute, so create object as normal and pass to createTaskEl 
+    else  {
+        var taskDataObj =  {
+            name:  taskNameInput,
+            type:  taskTypeInput
+
+        };
+     createTaskEl(taskDataObj);   
+    }
 
 
 
